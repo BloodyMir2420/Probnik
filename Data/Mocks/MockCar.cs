ca84c01aa47e32082671f424b9d65fcc263ce5a3
BloodyMir2420 <72360643+BloodyMir2420@users.noreply.github.com>
2020-10-12 11:58:50 +0300

    Add files via upload

---
+++ Data/Mocks/MockCar.cs
@@ -0,0 +1,52 @@
+﻿using probnik.Data.Interfaces;
+using probnik.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace probnik.Data.Mocks
+{
+    public class MockCar : IAllCars
+    {
+        private readonly ICarsCategory CategoryCars = new MockCategory();
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car
+                    {
+                        name = "Tesla",
+                        shortDescription = "Elon Musk`s shit",
+                        longDescription = "Elon Musk`s REAL shit",
+                        Image = "/img/TESLA.jpg",
+                        price = 45000,
+                        isFavorite = true,
+                        availeble = true,
+                        Category = this.CategoryCars.AllCategories.First()
+                    },
+                    new Car
+                    {
+                        name = "Lamorghini",
+                        shortDescription = "It is fast",
+                        longDescription = "It is real fucking fast",
+                        Image = "/img/LAMBA.jpg",
+                        price = 55000,
+                        isFavorite = false,
+                        availeble = true,
+                        Category = this.CategoryCars.AllCategories.Last()
+                    }
+                };
+            }
+        }
+
+        public IEnumerable<Car> GetFavoriteCars { get; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Mocks/MockCategory.cs
@@ -0,0 +1,25 @@
+﻿using probnik.Data.Interfaces;
+using probnik.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace probnik.Data.Mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+
+        public IEnumerable<Category> AllCategories 
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category { categoryName = "Электромашина", description = "Машина с электромотором"},
+                    new Category { categoryName = "Бензомашина", description = "Пизда тебе, экология ебаная"}
+                };
+            }
+        }
+    }
+}

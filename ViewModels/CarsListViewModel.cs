a6a41dd7cf03ecc2d9e27e7cdc77b457a0f3c520
BloodyMir2420 <72360643+BloodyMir2420@users.noreply.github.com>
2020-10-12 12:09:38 +0300

    Add files via upload

---
+++ ViewModels/CarsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using probnik.Data.Interfaces;
+using probnik.Data.Models;
+using probnik.Contollers;
+
+namespace probnik.ViewModels
+{
+    public class CarsListViewModel // Класс для просмотра списка машин
+    {
+        public IEnumerable<Car> GetAllCars { get; set; } // Перечисление всех машин (нужно убрать с название Get, но мне впадло)
+        public string CurrentCategory { get; set; } // Текущая категория, с котой ведется работа
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using probnik.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace probnik.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> FavoriteCars { get; set; }
+    }
+}
